d3b0f3ee363d480a186340a2a43316f05d5a5e3f
Alexander Frick <Alex313031@gmail.com>
2026-05-30 22:44:50 -0500

    fixups to scripts handling of dirs

--- assets/buildwavs.sh
+++ assets/buildwavs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-export HERE=${PWD} &&
-export TARGET=../src/res &&
+# Current dir (where this script lives, not where it was invoked)
+export HERE=$(cd "$(dirname "$0")" && pwd) &&
 
+# Go up one dir to root of repo
+export ROOT="$(dirname -- "$HERE")"
+
+# src resources directory
+export TARGET=${ROOT}/src/res &&
+
+# Convert files into Windows 2000+ compatible .wavs, with custom volume
 ffmpeg -y -i ${HERE}/tada.wav -filter:a "volume=0.60" -c:a adpcm_ms -ar 44100 -ac 2 ${TARGET}/tada.wav &&
 
 ffmpeg -y -i ${HERE}/imgburn_ohno.wav -filter:a "volume=0.60" -c:a adpcm_ms -ar 44100 -ac 2 ${TARGET}/ohno.wav &&
--- clang_format.sh
+++ clang_format.sh
@@ -2,12 +2,27 @@
 
 # Copyright (c) 2026 Alex313031.
 
-export HERE=${PWD} &&
+YEL='\033[1;33m' # Yellow
+CYA='\033[1;96m' # Cyan
+RED='\033[1;31m' # Red
+GRE='\033[1;32m' # Green
+c0='\033[0m' # Reset Text
+bold='\033[1m' # Bold Text
+underline='\033[4m' # Underline Text
 
+# Current dir (where this script lives, not where it was invoked)
+export HERE=$(cd "$(dirname "$0")" && pwd) &&
+
+# Format file with rules
 export CLANG_FORMAT_FILE=${HERE}/.clang-format &&
 
-clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/logging/{*.h,*.cc} &&
+printf "${GRE} Clang formatting using ${CLANG_FORMAT_FILE} ${c0}\n" &&
 
+printf "${CYA} clang-format src/logging ${c0}\n" &&
+# Logging dir first
+clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/logging/{*.h,*.cc} &&
+printf "${CYA} clang-format src/ ${c0}\n" &&
+# Main source
 clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/{*.h,*.cc} &&
 
 exit 0
